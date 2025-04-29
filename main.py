18b3381786de88cc36ad64f3c833a3b91e6b2348
Gesley de Oliveira Rosa <127444907+GesleyOliveira@users.noreply.github.com>
2025-04-29 16:30:22 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import os
+import pandas as pandas
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import yagmail
+from dotenv import load_dotenv
